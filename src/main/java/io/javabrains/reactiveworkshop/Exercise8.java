380f45fde47a9f106a1ad3d39f8693eaaf115456
Abdalrhman Mostafa <mno3aat333@gmail.com>
2024-10-25 11:10:02 +0300

    Implemented Exercises 8 and 9 in Reactive Workshop, adding error handling and aggregation logic to Flux pipelines.

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -1,8 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
-import reactor.core.publisher.Flux;
-
 import java.io.IOException;
+
+import reactor.core.publisher.Flux;
 
 public class Exercise8 {
 
@@ -12,14 +12,23 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+        .subscribe(e -> System.out.println(e), err -> System.out.println("Error : " + err.getMessage())); //** catch the error
+        //** Another Method
+        ReactiveSources.intNumbersFluxWithException()
+        .doOnError(err -> System.out.println("Error : " + err.getMessage())) //** pass the error and do action 
+        .subscribe(e -> System.out.println(e)); 
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+       ReactiveSources.intNumbersFluxWithException()
+        .onErrorContinue((err,item) -> System.out.println("Error : " + err.getMessage())) //** continue after handling an error , and the item is who made the exception
+        .subscribe(e -> System.out.println(e)); 
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException()
+        .onErrorResume(err -> Flux.just(-1,-2)) //** when error happen , replace with a fallback sequence (switch flux)
+        .subscribe(e -> System.out.println(e)); 
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,19 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+       ReactiveSources.intNumbersFlux()
+       .count()
+       .subscribe(System.out::println); //** returns mono long of size when the flux completed
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
-
+        ReactiveSources.intNumbersFlux()
+        .collectList()//** returns Mono List of items when finished
+        .subscribe(System.out::println);
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+        .buffer(2)//** take 2 cumulative numbers to do an operation on them (every 2 number of main flux returns 1 event of the new flux)
+        .map(list -> list.get(0) + list.get(1)) //** get the sum
+        .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
